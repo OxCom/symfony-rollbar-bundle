7443c273d4987bac5db44792ff69c317889c9424
OxCom <gm.oxcom@gmail.com>
2018-05-28 19:48:37 +0200

    fix: update tests to handle prioritets on events

--- Tests/SymfonyRollbarBundle/BundleTest.php
+++ Tests/SymfonyRollbarBundle/BundleTest.php
@@ -3,6 +3,7 @@ namespace SymfonyRollbarBundle\Tests;
 
 use Symfony\Bridge\PhpUnit\DeprecationErrorHandler;
 use Symfony\Bundle\FrameworkBundle\Test\KernelTestCase;
+use Symfony\Component\EventDispatcher\ContainerAwareEventDispatcher;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpKernel\Debug\TraceableEventDispatcher;
 use \Symfony\Component\HttpKernel\Event\GetResponseForExceptionEvent;
@@ -29,16 +30,23 @@ class BundleTest extends KernelTestCase
          * @var TraceableEventDispatcher $eventDispatcher
          */
         $eventDispatcher = $container->get('event_dispatcher');
-        $listeners = $eventDispatcher->getListeners('kernel.exception');
+        $listeners       = $eventDispatcher->getListeners('kernel.exception');
 
         $expected = [
-            \SymfonyRollbarBundle\EventListener\AbstractListener::class,
-            \Symfony\Component\HttpKernel\EventListener\ExceptionListener::class,
+            \SymfonyRollbarBundle\EventListener\ErrorListener::class,
+            \SymfonyRollbarBundle\EventListener\ExceptionListener::class,
         ];
 
-        foreach ($listeners as $listener) {
-            $ok = $listener[0] instanceof $expected[0] || $listener[0] instanceof $expected[1];
-            $this->assertTrue($ok, 'Listeners were not registered');
+        foreach ($expected as $class) {
+            $found = false;
+            foreach ($listeners as $listener) {
+                if ($listener[0] instanceof $class) {
+                    $found = true;
+                    break;
+                }
+            }
+
+            $this->assertTrue($found, "Listeners {$class} was not registered");
         }
 
         restore_error_handler();
--- Tests/SymfonyRollbarBundle/EventListener/AbstractListenerTest.php
+++ Tests/SymfonyRollbarBundle/EventListener/AbstractListenerTest.php
@@ -21,29 +21,6 @@ class AbstractListenerTest extends KernelTestCase
         static::bootKernel();
     }
 
-    public function testListeners()
-    {
-        $container = static::$kernel->getContainer();
-
-        /**
-         * @var TraceableEventDispatcher $eventDispatcher
-         */
-        $eventDispatcher = $container->get('event_dispatcher');
-        $listeners = $eventDispatcher->getListeners('kernel.exception');
-
-        $expected = [
-            \SymfonyRollbarBundle\EventListener\AbstractListener::class,
-            \Symfony\Component\HttpKernel\EventListener\ExceptionListener::class,
-        ];
-
-        foreach ($listeners as $listener) {
-            $ok = $listener[0] instanceof $expected[0] || $listener[0] instanceof $expected[1];
-            $this->assertTrue($ok, 'Listeners were not registered');
-        }
-
-        restore_error_handler();
-    }
-
     /**
      * @return array
      */
