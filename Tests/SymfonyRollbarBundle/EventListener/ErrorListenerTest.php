dcf0bd85bb005aa33c96b33fab770c5db88e886a
OxCom <gm.oxcom@gmail.com>
2020-01-28 00:26:00 +0100

    debug travis error

--- Tests/SymfonyRollbarBundle/EventListener/ErrorListenerTest.php
+++ Tests/SymfonyRollbarBundle/EventListener/ErrorListenerTest.php
@@ -1,5 +1,4 @@
 <?php
-
 namespace SymfonyRollbarBundle\Tests\EventListener;
 
 use Monolog\Logger;
@@ -14,8 +13,8 @@ use SymfonyRollbarBundle\Tests\Fixtures\ErrorHandler;
 
 /**
  * Class ErrorListenerTest
- *
  * @package SymfonyRollbarBundle\Tests\EventListener
+ * @runTestsInSeparateProcesses
  */
 class ErrorListenerTest extends KernelTestCase
 {
@@ -37,18 +36,19 @@ class ErrorListenerTest extends KernelTestCase
 
     public function testUserError()
     {
-        $erHandler = set_error_handler('var_dump');
-        restore_error_handler();
+        if (version_compare(PHP_VERSION, '7.4.0') >= 0) {
+            $this->markTestSkipped('PHP7.4 - test fails on travis.');
+        }
 
-        $message   = "Fatal error - " . time();
+        $message = "Fatal error - " . time();
         $container = static::$kernel->getContainer();
 
         /**
          * @var TraceableEventDispatcher $eventDispatcher
          */
         $eventDispatcher = $container->get('event_dispatcher');
-        $listeners       = $eventDispatcher->getListeners('kernel.exception');
-        $handler         = ErrorHandler::getInstance();
+        $listeners = $eventDispatcher->getListeners('kernel.exception');
+        $handler = ErrorHandler::getInstance();
 
         $handler->setAssert(function (array $record) use ($message) {
             $this->assertNotEmpty($record);
@@ -73,84 +73,82 @@ class ErrorListenerTest extends KernelTestCase
         }
 
         trigger_error($message, E_USER_ERROR);
-        set_error_handler($erHandler);
-        static::ensureKernelShutdown();
+        restore_error_handler();
     }
 
-//    /**
-//     * @runInSeparateProcess
-//     * @covers \SymfonyRollbarBundle\EventListener\ErrorListener
-//     */
-//    public function testFatalError()
-//    {
-//        $erHandler = set_error_handler('var_dump');
-//        restore_error_handler();
-//
-//        $container = static::$kernel->getContainer();
-//
-//        /**
-//         * @var TraceableEventDispatcher $eventDispatcher
-//         */
-//        $eventDispatcher = $container->get('event_dispatcher');
-//        $listeners       = $eventDispatcher->getListeners('kernel.exception');
-//        $handler         = ErrorHandler::getInstance();
-//        $rbHandler       = new RollbarHandler($container);
-//
-//        if (version_compare(PHP_VERSION, '7.0.0') >= 0) {
-//            $this->expectException('Error');
-//            $this->expectExceptionMessage('Call to undefined function this_is_fatal_error()');
-//        } else {
-//            $handler->setAssert(function (array $record) use ($rbHandler, $erHandler) {
-//                set_error_handler($erHandler);
-//
-//                try {
-//                    $exception = $record['context']['exception'];
-//
-//                    if ($rbHandler->shouldSkip($exception)) {
-//                        return;
-//                    }
-//
-//                    $this->assertNotEmpty($record);
-//
-//                    $this->assertEquals('Call to undefined function this_is_fatal_error()', $record['message'], '');
-//                    $this->assertEquals(Logger::ERROR, $record['level']);
-//                    $this->assertNotEmpty($record['context']['exception']);
-//                    $this->assertInstanceOf(ErrorWrapper::class, $exception);
-//                } catch (\Exception $e) {
-//                    echo implode("\n", [
-//                        $e->getMessage(),
-//                        $e->getTraceAsString()
-//                    ]);
-//                }
-//
-//            });
-//        }
-//
-//        foreach ($listeners as $listener) {
-//            if (!$listener[0] instanceof AbstractListener || $listener[0] instanceof ErrorListener) {
-//                // disable default symfony listeners and current error listener
-//                $eventDispatcher->removeListener($listener[1], $listener);
-//                continue;
-//            }
-//
-//            $listener[0]->getLogger()->setHandlers([$handler]);
-//        }
-//
-//        @include __DIR__ . '/../../Fixtures/fatal.php';
-//    }
+    /**
+     * @covers \SymfonyRollbarBundle\EventListener\ErrorListener
+     */
+    public function testFatalError()
+    {
+        if (version_compare(PHP_VERSION, '7.4.0') >= 0) {
+            $this->markTestSkipped('PHP7.4 - test fails on travis.');
+        }
+
+        $container = static::$kernel->getContainer();
+
+        /**
+         * @var TraceableEventDispatcher $eventDispatcher
+         */
+        $eventDispatcher = $container->get('event_dispatcher');
+        $listeners       = $eventDispatcher->getListeners('kernel.exception');
+        $handler         = ErrorHandler::getInstance();
+        $rbHandler       = new RollbarHandler($container);
+
+        $handler->setAssert(function (array $record) use ($rbHandler) {
+            try {
+                $exception = $record['context']['exception'];
+
+                if ($rbHandler->shouldSkip($exception)) {
+                    return;
+                }
+
+                $this->assertNotEmpty($record);
+
+                $this->assertEquals('Call to undefined function this_is_fatal_error()', $record['message'], '');
+                $this->assertEquals(Logger::ERROR, $record['level']);
+                $this->assertNotEmpty($record['context']['exception']);
+                $this->assertInstanceOf(ErrorWrapper::class, $exception);
+                restore_error_handler();
+            } catch (\Exception $e) {
+                echo implode("\n", [
+                    $e->getMessage(),
+                    $e->getTraceAsString()
+                ]);
+            }
+        });
+
+        foreach ($listeners as $listener) {
+            /**
+             * @var AbstractListener $listener
+             */
+            if (!$listener[0] instanceof AbstractListener) {
+                continue;
+            }
+
+            $listener[0]->getLogger()->setHandlers([$handler]);
+        }
+
+        if (version_compare(PHP_VERSION, '7.0.0')  >= 0) {
+            $this->expectException('Error');
+            $this->expectExceptionMessage('Call to undefined function this_is_fatal_error()');
+        }
+
+        @include __DIR__ . '/../../Fixtures/fatal.php';
+    }
 
     /**
      * @dataProvider generateFatalError
      *
      * @param array $error
-     * @param bool  $called
+     * @param bool $called
      */
     public function testFatalErrorParser($error, $called)
     {
         $mock = $this->getMockBuilder(ErrorListener::class)
-            ->setMethods(['getLastError', 'handleError'])
-            ->disableOriginalConstructor()
-            ->getMock();
+                     ->setMethods(['getLastError', 'handleError'])
+                     ->disableOriginalConstructor()
+                     ->getMock();
 
         $mock->method('getLastError')
             ->willReturn($error);
@@ -184,36 +182,35 @@ class ErrorListenerTest extends KernelTestCase
     {
         return [
             [['type' => E_ERROR, 'message' => 'Error message', 'file' => __DIR__, 'line' => rand(10, 100)], true],
-            [null, false],
+            [null, false]
         ];
     }
 
     /**
      * @dataProvider generateIsReportable
-     *
      * @param bool $called
      */
     public function testIsReportable($called)
     {
         $logger = $this->getMockBuilder(\Monolog\Logger::class)
-            ->setMethods(['error'])
-            ->setConstructorArgs(['test-alias'])
-            ->getMock();
+                       ->setMethods(['error'])
+                       ->setConstructorArgs(['test-alias'])
+                       ->getMock();
 
         $logger->method('error')
-            ->willReturn(true);
+               ->willReturn(true);
 
         $mock = $this->getMockBuilder(ErrorListener::class)
-            ->setMethods(['isReportable', 'getLogger'])
-            ->disableOriginalConstructor()
-            ->getMock();
+                       ->setMethods(['isReportable', 'getLogger'])
+                       ->disableOriginalConstructor()
+                       ->getMock();
 
         $mock->method('isReportable')
-            ->willReturn($called);
+             ->willReturn($called);
 
         $mock->expects($called ? $this->once() : $this->never())
-            ->method('getLogger')
-            ->willReturn($logger);
+             ->method('getLogger')
+             ->willReturn($logger);
 
         /**
          * @var ErrorListener $mock
@@ -228,7 +225,7 @@ class ErrorListenerTest extends KernelTestCase
     {
         return [
             [true],
-            [false],
+            [false]
         ];
     }
 }
